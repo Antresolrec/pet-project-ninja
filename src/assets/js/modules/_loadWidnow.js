937f6940a4e61a30dfff9652719cbb320bd338db
AntresolRec <AntresolRec@yandex.ru>
2022-04-29 15:45:55 +0300

    v1.7

--- src/assets/js/modules/_loadWidnow.js
+++ src/assets/js/modules/_loadWidnow.js
@@ -1,7 +1,7 @@
 import setScroll from './scroll';
 import scrollHeader from './header-scroll';
 
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.wrapper').classList.add('_loaded');
   document.body.classList.remove('off-animations');
   setScroll();
