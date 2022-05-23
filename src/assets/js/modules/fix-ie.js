6a1411d2127ea6f6344eef6013b7721317795206
AntresolRec <AntresolRec@yandex.ru>
2022-05-23 11:58:09 +0300

    v2.1

--- src/assets/js/modules/fix-ie.js
+++ src/assets/js/modules/fix-ie.js
@@ -1,9 +1,9 @@
-let uA = window.navigator.userAgent;
-function isIE() {
-  uA = navigator.userAgent;
-  const isIe = uA.indexOf('MSIE ') > -1 || uA.indexOf('Trident/') > -1;
-  return isIe;
-}
-if (isIE()) {
-  document.querySelector('html').classList.add('fix-ie');
-}
+// let uA = window.navigator.userAgent;
+// function isIE() {
+//   uA = navigator.userAgent;
+//   const isIe = uA.indexOf('MSIE ') > -1 || uA.indexOf('Trident/') > -1;
+//   return isIe;
+// }
+// if (isIE()) {
+//   document.querySelector('html').classList.add('fix-ie');
+// }
